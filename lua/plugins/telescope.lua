59103329092bd54d4d55094c4a2f7f3936712451
JEEJY30 <gio1818ggg@gmail.com>
2025-12-19 15:50:53 +0400

    nvim-tree to telescope only

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,13 +4,27 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-telescope/telescope-file-browser.nvim',
   },
   config = function()
     local telescope = require('telescope')
-    telescope.setup({})
+    telescope.setup({
+      defaults = {
+        initial_mode = "normal",
+      },
+      extensions = {
+        file_browser = {
+          hijack_netrw = true,
+          hidden = true,
+        },
+      },
+    })
     telescope.load_extension('fzf')
-    
+    telescope.load_extension('file_browser')
+
     local keymap = vim.keymap
+    -- Find files in current directory and all subdirectories
+    keymap.set('n', '<leader>e', '<cmd>Telescope find_files cwd=%:p:h<CR>', { desc = 'Find files in current dir' })
     keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { desc = 'Find files' })
     keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { desc = 'Live grep' })
     keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { desc = 'Find buffers' })
