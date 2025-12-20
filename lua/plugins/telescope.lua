0a52a503c69b64652883b79f66dc700fad775890
JEEJY30 <gio1818ggg@gmail.com>
2025-12-20 09:52:48 +0400

    Telescope of telescopes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,14 +8,36 @@ return {
   },
   config = function()
     local telescope = require('telescope')
+    local fb_actions = require('telescope').extensions.file_browser.actions
+    local actions = require('telescope.actions')
+
     telescope.setup({
       defaults = {
-        initial_mode = "normal",
+        initial_mode = "normal",  -- Default to normal mode
+        mappings = {
+          n = {
+            ["i"] = "to_fuzzy_refine",  -- Press 'i' to search in normal mode
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          initial_mode = "insert",  -- Start in insert mode for find_files
+        },
+        live_grep = {
+          initial_mode = "insert",  -- Start in insert mode for live_grep
+        },
       },
       extensions = {
         file_browser = {
           hijack_netrw = true,
           hidden = true,
+          mappings = {
+            ["n"] = {
+              ["h"] = fb_actions.goto_parent_dir,
+              ["l"] = actions.select_default,
+            },
+          },
         },
       },
     })
@@ -23,8 +45,7 @@ return {
     telescope.load_extension('file_browser')
 
     local keymap = vim.keymap
-    -- Find files in current directory and all subdirectories
-    keymap.set('n', '<leader>e', '<cmd>Telescope find_files cwd=%:p:h<CR>', { desc = 'Find files in current dir' })
+    keymap.set('n', '<leader>e', '<cmd>Telescope file_browser path=%:p:h<CR>', { desc = 'File browser' })
     keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { desc = 'Find files' })
     keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { desc = 'Live grep' })
     keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { desc = 'Find buffers' })
