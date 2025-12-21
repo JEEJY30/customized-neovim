b84aeb19686dccdbaca1f87af4c684c90b1c9a46
JEEJY30 <gio1818ggg@gmail.com>
2025-12-21 13:40:06 +0400

    Lazy-git 0 exit fix

--- lua/plugins/lazy-git.lua
+++ lua/plugins/lazy-git.lua
@@ -1,21 +1,26 @@
--- nvim v0.8.0
 return {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+  'kdheepak/lazygit.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', { desc = 'Open LazyGit' })
+
+    -- Auto-close the terminal buffer when lazygit exits
+    vim.api.nvim_create_autocmd("TermClose", {
+      pattern = "*lazygit*",
+      callback = function()
+        -- Small delay to let things settle
+        vim.defer_fn(function()
+          -- Find and delete the lazygit buffer
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            local buf_name = vim.api.nvim_buf_get_name(buf)
+            if buf_name:match("lazygit") then
+              pcall(vim.api.nvim_buf_delete, buf, { force = true })
+            end
+          end
+        end, 10)
+      end,
+    })
+  end,
 }
